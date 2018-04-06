c9ac0d9f34334930d437dd02fe87f8b50217d766
Ramon Castan <ramon.castan175@gmail.com>
2018-04-06 13:12:44 -0300

    initial commit

---
+++ app/src/main/java/com/example/ramon/quizzapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.ramon.quizzapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    
+    private EditText playerName;
+    private Button playBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        playBtn = findViewById(R.id.button_play);
+        playerName = findViewById(R.id.editText_playerName);
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = playerName.getText().toString();
+
+                if(name.equals("")){
+                    Toast.makeText(MainActivity.this, "Insira seu nome para prosseguir!", Toast.LENGTH_SHORT).show();
+                }else{
+
+                    Toast.makeText(MainActivity.this, "Quizz iniciado!", Toast.LENGTH_SHORT).show();
+
+                    Intent i = new Intent(MainActivity.this,GameActivity.class);
+                    i.putExtra("playerName", name);
+                    startActivity(i);
+
+
+                }
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/ramon/quizzapp/models/Questions.java
@@ -0,0 +1,11 @@
+package com.example.ramon.quizzapp.models;
+
+import java.util.List;
+
+public class Questions {
+
+    private List<Question> questions;
+
+
+
+}
